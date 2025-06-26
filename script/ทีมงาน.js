066fa442c4958dac6cd38ae09cc0d972eb9dd351
Khunakon Thamdi <kingmanstar55@gmail.com>
2025-06-26 13:56:09 +0700

    ggdhw-

--- script/ทีมงาน.js
+++
@@ -1,9 +0,0 @@
-function showPage(pageNumber) {
-  const pages = document.querySelectorAll('.page');
-  pages.forEach((page, index) => {
-    page.style.display = (index === pageNumber - 1) ? 'block' : 'none';
-  });
-}
-
-// Show page 1 by default
-showPage(1);
